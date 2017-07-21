9a81997eafe26770e9d14f5d6c6a01c7ffd959a3
Crypto72 <antrom1972@yahoo.co.uk>
2017-07-21 21:36:11 +1200

    Add files via upload

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid AUXILIUM_NETWORK = { { 0x17, 0x1a, 0x69, 0x1c, 0x72, 0x1f, 0x1e, 0x2f, 0x11, 0x1c, 0x45, 0x00, 0xff, 0x25, 0x1b, 0x44 } };
+}
